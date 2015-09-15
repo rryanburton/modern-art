3fd2db19d736c27407600de0aa6a28a179f250b3
Ryan Burton <rryanburton@gmail.com>
2015-09-14 22:33:15 -0400

    favorite art expression

--- random_art.py
+++ random_art.py
@@ -1,16 +1,19 @@
 import random
-
+from math import *
 # Your job is to create better version of create_expression and
 # run_expression to create random art.
 # Your expression should have a __str__() function defined for it.
 
-
+# sin = math.sin
+# cos = math.cos
+# tan = math.tan
+# pi = math.pi
 def create_expression():
     """This function takes no arguments and returns an expression that
     generates a number between -1.0 and 1.0, given x and y coordinates."""
-    expr1 = lambda x, y: (x + y)/2
-    expr2 = lambda x, y: (x - y)/2
-    expr3 = lambda x, y: x * y
+    expr1 = lambda x, y: sin(x) + cos(y) + sin(x) + cos(sin(tan(x)))
+    expr2 = lambda x, y: cos(tan(y))
+    expr3 = lambda x, y: sin(y) + cos(x) + sin(x) + cos(sin(tan(y)))
     return random.choice([expr1, expr2, expr3])
 #
 # res = []
